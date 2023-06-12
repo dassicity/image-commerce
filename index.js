34124fe24e28f70e53965d5cb326dfebeecce1ee
Soumyanil Das <dassic@outlook.com>
2023-06-13 00:36:21 +0530

    Added documentation of signin/signup using swagger

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes')
 const PORT = process.env.PORT;
 
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
 const app = express();
 
 // middlewares
@@ -14,7 +17,24 @@ app.use(express.json());
 app.use(express.static('content'));
 app.use(express.urlencoded({ extended: false }));
 
+//swagger init
+const specs = swaggerJsDoc({
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Photo Shop API",
+            version: "1.0.0",
+            description: "Buy/Sell Photos"
+        },
+        servers: [{
+            url: process.env.BASE_URL,
+        }],
+    },
+    apis: ["./routes/*.js", "./controllers/*.js"]
+});
+
 // routing middlewares
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/product', productRoutes);
 
